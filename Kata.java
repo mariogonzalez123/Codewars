ba9974e5510a00161fe0d7f3aeb6e114e6c9b9ab
mariogonzalez123 <58482165+mariogonzalez123@users.noreply.github.com>
2023-07-05 12:45:59 +0200

    Add files via upload

---
+++ Kata.java
@@ -0,0 +1,7 @@
+public class Kata
+ {
+  public static int square(int n){
+    return 3;
+       //Your Code
+  }
+ }
---
+++ LineNumbering.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+public class LineNumbering {
+    public static List<String> number(List<String> lines) {
+        return Arrays.asList("a","b");
+    }
+}
